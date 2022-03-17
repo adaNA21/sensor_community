e045f3bb04ffa4e5a719d59c7aeb77abe3a9bd50
A. Di Antonio <andrea@newautomotive.org>
2022-03-17 18:12:47 +0000

    Include first module section to download the data

---
+++ modules/Modules.py
@@ -0,0 +1,42 @@
+import datetime
+import pandas as pd
+from tqdm import tqdm
+from dateutil.relativedelta import relativedelta
+from modules import dic_sensors, installation_date, initial_url
+
+
+class DownloadData:
+
+    def __init__(self):
+        self.initial_url = initial_url
+        self.installation_date = installation_date
+        self.dic_sensors = dic_sensors
+
+    def download_data(self, index):
+
+        if str.lower(index) == 'pm' or str.lower(index) == 'meteo':
+            try:
+                df_data_all = pd.read_csv(f'data/{str.upper(index)}_data.csv', index_col=0, parse_dates=True)
+                latest_date = df_data_all.index.max() + relativedelta(days=1)
+            except FileNotFoundError:
+                df_data_all = pd.DataFrame()
+                latest_date = self.installation_date
+
+            date_interval = (datetime.datetime.now() - latest_date).days
+
+            for i in tqdm(range(date_interval)):
+                date_format = f'{latest_date.year}-{"{:02d}".format(latest_date.month)}-' \
+                              f'{"{:02d}".format(latest_date.day)}'
+
+                data_url = f'{self.initial_url}{date_format}/{date_format}_{self.dic_sensors[str.upper(index)]}.csv'
+
+                df_data = pd.read_csv(data_url, sep=';')
+                df_data = df_data.set_index(pd.to_datetime(df_data['timestamp'])).drop('timestamp', axis=1)
+                df_data_all = pd.concat([df_data_all, df_data])
+
+                latest_date += relativedelta(days=1)
+
+            df_data_all.to_csv(f'data/{str.upper(index)}_data.csv')
+            return df_data_all
+        else:
+            return f"ERROR: {index} value not valid!"
--- modules/__init__.py
+++ modules/__init__.py
@@ -1,11 +1,16 @@
 import datetime
+
 # Sensors id
 pm_sensor_id = 70241
-temp_sensor_id = 70242
+weather_sensor_id = 70242
 
 # Sensors names
 pm_sensor_name = f'pms5003_sensor_{pm_sensor_id}'
-temp_sensor_name = f'bme280_sensor_{temp_sensor_id}'
+meteo_sensor_name = f'bme280_sensor_{weather_sensor_id}'
+dic_sensors = {'PM': pm_sensor_name, 'METEO': meteo_sensor_name}
 
-# Date when the settings was installed
+# Date when the setting was installed
 installation_date = datetime.datetime.strptime('2022-02-08', '%Y-%m-%d')
+
+# Url were sensor community data are stored
+initial_url = "https://archive.sensor.community/"
